359cb5c306d91f11d1b18648428ad402d9ad060b
Amy Rippeto <amyrippeto@gmail.com>
2019-09-15 14:21:54 -0600

    Add basic html, scss and js structure, fetch data

--- src/index.js
+++ src/index.js
@@ -1,13 +1,49 @@
-// This is the JavaScript entry file - your code begins here
-// Do not delete or rename this file ********
-
 // An example of how you import jQuery into a JS file if you use jQuery in that file
 import $ from 'jquery';
+import Hotel from '../src/Hotel';
 
-// An example of how you tell webpack to use a CSS (SCSS) file
 import './css/base.scss';
 
 // An example of how you tell webpack to use an image (also need to link to it in the index.html)
-import './images/turing-logo.png'
+// import './images/turing-logo.png'
+let date, today, hotel, roomsData, bookingsData, ordersData, customersData
 
-console.log('This is the JavaScript entry file - your code begins here.');
+date = new Date().toISOString().replace('-', '/').split('T')[0].replace('-', '/');
+today = new Date().toString().split(' ').slice(0, 4).join(' ');
+
+roomsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/rooms/rooms');
+bookingsData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/bookings/bookings');
+ordersData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/room-services/roomServices');
+customersData = fetch('https://fe-apps.herokuapp.com/api/v1/overlook/1904/users/users');
+
+Promise.all([roomsData, bookingsData, ordersData, customersData])
+    .then(responses => Promise.all(responses.map(response => response.json())))
+    .then(data => createHotel(date, data[0].rooms, data[1].bookings, data[2].roomServices, data[3].users))
+    .catch(error => console.log(error));
+
+
+const createHotel = (date, rooms, bookings, roomServices, customers) => {
+  hotel = new Hotel(date, rooms, bookings, roomServices, customers);
+  $('#today').text(today);
+}
+
+$(document).ready(() => {
+  // $('#page').hide();
+  
+  // Show the first tab by default
+  $('.tabs-stage div').hide();
+  $('.tabs-stage div:first').show();
+  $('.tabs-nav li:first').addClass('tab-active');
+  
+  // Change tab class and display content
+  $('.tabs-nav a').on('click', function(event){
+    event.preventDefault();
+    $('.tabs-nav li').removeClass('tab-active');
+    $(this).parent().addClass('tab-active');
+    $('.tabs-stage div').hide();
+    $($(this).attr('href')).show();
+  });
+
+});
+
+
